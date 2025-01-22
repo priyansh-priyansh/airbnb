2f779816f2c0bc41b09d3b710adda0b40165be94
priyansh-priyansh <red.priyansh@gmail.com>
2025-01-22 20:43:50 +0530

    initialize Database

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const Listing = require("./models/listing.js");
 
+const Listing = require("./models/listing.js");
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
   .then(() => {
     console.log("Connect to DB");
