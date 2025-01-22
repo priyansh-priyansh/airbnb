56bc1ee5f52b70df05b5d3bc72bc7a50411ee3d9
priyansh-priyansh <red.priyansh@gmail.com>
2025-01-23 00:08:00 +0530

    read

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ async function main() {
 
 app.set("view engine" , "ejs");
 app.set("views" , path.join(__dirname,"views"));
+app.use(express.urlencoded({extended : true}));
 
 app.get("/", (req, res) => {
   res.send("Hi i am root");
@@ -28,6 +29,13 @@ app.get("/listings", async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index", { allListings });
   });
+
+
+app.get("/listings/:id" , async (req , res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs" , {listing});
+});
   
 
 // app.get("/testListing", async (req, res) => {
